b627bc7191825d51603f6ad2de206cdb06e542bb
Shiyu (Baran) Liu <liushiyu1994@gmail.com>
2019-12-03 03:13:52 -0500

    Statistics of games bought or played by users.

---
+++ steam/src/shiyu_users_items_code/config.py
@@ -0,0 +1,9 @@
+data_folder = 'steam/data'
+raw_data_folder = '{}/raw_data'.format(data_folder)
+
+test_users_items_file = '{}/australian_users_items.example.json'.format(data_folder)
+users_items_file = '{}/australian_users_items.json'.format(raw_data_folder)
+test_steam_games_file = '{}/steam_games.example.json'.format(data_folder)
+steam_games_file = '{}/steam_games.json'.format(raw_data_folder)
+
+game_stat_file = '{}/game_stat_files.csv'.format(data_folder)
