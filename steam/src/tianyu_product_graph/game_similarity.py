baa212aefeed3391298fe7a403940e65e000f85c
Tianyu Dai <td115@duke.edu>
2020-02-02 17:08:01 -0500

    add train/test

---
+++ steam/src/tianyu_product_graph/game_similarity.py
@@ -0,0 +1,10 @@
+import numpy as np
+from . import dataset_divider
+
+class GameSimilarity: 
+    def __init__(self, spec): 
+        self.train_game_id = []
+
+if __name__ == '__main__': 
+    data_organizer = dataset_divider.DataOrganizer('tags')
+    data_organizer.readRawData()
