41481fa8639a658724daccd60d7c2d65fea9d887
TianyuDai <td115@duke.edu>
2020-02-04 01:29:07 -0500

    add comparison with co-purchase data

--- steam/src/tianyu_product_graph/co-purchase_similarity.py
+++ steam/src/tianyu_product_graph/co-purchase_similarity.py
@@ -1,9 +1,11 @@
 import numpy as np
 import ast
+import pandas as pd
+import pickle
 import dataset_divider
 
-def userDataReader(fname): 
-    user_data_handler = open(fname, 'r')
+def userDataReader(infname, outfname): 
+    user_data_handler = open(infname, 'r')
     purchase_game_id = []
     for line in user_data_handler: 
         line = ast.literal_eval(line)
@@ -12,25 +14,30 @@ def userDataReader(fname):
         for item in items:
             user_game_id.append(item['item_id'])
         purchase_game_id.append(user_game_id)
-    # print(purchase_game_id)
-    np.savetxt('co-purchase_game_id.txt', purchase_game_id, fmt='%s')
+    with open(outfname, 'wb') as f: 
+        pickle.dump(purchase_game_id, f)
 
-def coPurchaseSimilarity(test_game_id):
-    purchase_game_id = open('co-purchase_game_id.txt')
-    # purchase_game_id = np.genfromtxt('co-purchase_game_id.txt', dtype='str')
+def coPurchaseSimilarity(test_game_id, outfname):
+    with open(outfname, 'rb') as f: 
+        purchase_game_id = pickle.load(f)
     game_similarity = np.zeros((len(test_game_id), len(test_game_id)))
+    idk = 0
     for user in purchase_game_id: 
+        print(idk)
         for i in range(len(user)): 
             for j in range(i+1, len(user)): 
                 game_i = user[i]
                 game_j = user[j]
+                # print(game_i, game_j)
                 if game_i in test_game_id and game_j in test_game_id: 
                     game_similarity[test_game_id.index(game_i), test_game_id.index(game_j)] += 1
-    np.save(game_similarity, 'co-purchase_game_similarity')
+        idk += 1
+    np.save('co-purchase_game_similarity', game_similarity)
 
 if __name__ == '__main__': 
-    fname = '../../data/australian_users_items.example.json'
-    userDataReader(fname)
+    infname = '../../data/australian_users_items.json'
+    outfname = 'co-purchase_game_id.pkl'
+    userDataReader(infname, outfname)
     data_organizer = dataset_divider.DataOrganizer('tags')
     data_organizer.trainTestDivider()
-    coPurchaseSimilarity(data_organizer.test_game_id)
+    coPurchaseSimilarity(data_organizer.test_game_id, outfname)
--- steam/src/tianyu_product_graph/matrices_similarity.py
+++ steam/src/tianyu_product_graph/matrices_similarity.py
@@ -14,21 +14,22 @@ def matricesDistance(matrix_1, matrix_2):
     d = np.linalg.norm(matrix_1-matrix_2)
     return d
 
-def corrDistribution(arr, name): 
+def corrDistribution(arr, name_1, name_2): 
     plt.figure()
     plt.hist(arr, 50, density=True, range=(-1, 1))
     plt.axvline(x=0, color='black')
     plt.xlabel('correlation')
     plt.ylabel('N')
     plt.xlim(-1, 1)
-    plt.title('The correlation of game similarity between\n'+name)
-    plt.savefig('correlation_'+name)
+    plt.title('The correlation of game similarity between\n'+name_1+' and '+name_2)
+    plt.savefig('correlation_'+name_1+'_'+name_2)
 
 if __name__ == '__main__': 
     matrix_seller = np.load('seller_game_similarity.npy')
     matrix_buyer = np.load('buyer_game_similarity.npy')
     matrix_standard = np.load('standard_game_similarity.npy')
     matrix_purchase = np.load('seller_purchase_game_similarity.npy')
+    matrix_cop_standard = np.load('co-purchase_game_similarity.npy')
     """
     d_sb = matricesDistance(matrix_seller, matrix_buyer)
     d_st = matricesDistance(matrix_seller, matrix_standard)
@@ -36,21 +37,29 @@ if __name__ == '__main__':
     d_ps = matricesDistance(matrix_purchase, matrix_seller)
     d_pt = matricesDistance(matrix_purchase, matrix_standard)
     d_pb = matricesDistance(matrix_purchase, matrix_buyer)
-    """
+    
     c_sb = matricesCorrelation(matrix_seller, matrix_buyer)
     c_st = matricesCorrelation(matrix_seller, matrix_standard)
     c_bt = matricesCorrelation(matrix_buyer, matrix_standard)
     c_ps = matricesCorrelation(matrix_purchase, matrix_seller)
     c_pt = matricesCorrelation(matrix_purchase, matrix_standard)
     c_pb = matricesCorrelation(matrix_purchase, matrix_buyer)
-
-    # print(d_sb, d_st, d_bt, d_ps, d_pt, d_pb)
-    # print(c_sb, c_st, c_bt, c_ps, c_pt, c_pb)
-    corr_list = [c_sb, c_st, c_bt, c_ps, c_pt, c_pb]
-    name_list = ['seller-centric and buyer-centric', 'seller-centric and co-play standard', 'buyer-centric and co-play standard', 'seller-centric and seller-centric with co-purchase input', 'seller-centric with co-purchase input and co-play standard', 'buyer-centric and seller-centric with co-purchase input']
+    c_cp = matricesCorrelation(matrix_purchase, matrix_cop_standard)
+    c_cs = matricesCorrelation(matrix_seller, matrix_cop_standard)
+    c_ct = matricesCorrelation(matrix_cop_standard, matrix_standard)
+    c_cb = matricesCorrelation(matrix_cop_standard, matrix_buyer)
+    """
+    matrix_list = [matrix_seller, matrix_buyer, matrix_standard, matrix_purchase, matrix_cop_standard]
+    name_list = ['seller-centric', 'buyer-centric', 'co-play standard', 'seller-centric with co-purchase input', 'co-purchase standard']
+    for i in range(len(matrix_list)): 
+        for j in range(i+1, len(matrix_list)): 
+            corr = matricesCorrelation(matrix_list[i], matrix_list[j])
+            corrDistribution(corr, name_list[i], name_list[j])
+    """
+    corr_list = [c_sb, c_st, c_bt, c_ps, c_pt, c_pb, c_cp, c_cs, c_ct, c_cb]
+    name_list = ['seller-centric and buyer-centric', 'seller-centric and co-play standard', 'buyer-centric and co-play standard', 'seller-centric and seller-centric with co-purchase input', 'seller-centric with co-purchase input and co-play standard', 'buyer-centric and seller-centric with co-purchase input', 'seller-centric with co-purchase input and co-purchase standard', 'seller-centric and co-purchase standard', 'co-purchase standard and co-play standard', 'buyer-centric and co-purchase standard']
     for corr, name in zip(corr_list, name_list): 
         corrDistribution(corr, name)
-    """
     corrDistribution(c_sb, 'sb')
     corrDistribution(c_st, 'st')
     corrDistribution(c_bt, 'bt')
