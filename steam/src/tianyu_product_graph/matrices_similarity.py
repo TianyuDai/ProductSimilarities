77918a14cf61d87527bddb942744242713f3db4e
Tianyu Dai <td115@duke.edu>
2020-02-04 16:17:32 -0500

    small modification

--- steam/src/tianyu_product_graph/matrices_similarity.py
+++ steam/src/tianyu_product_graph/matrices_similarity.py
@@ -18,11 +18,11 @@ def corrDistribution(arr, name_1, name_2):
     plt.figure()
     plt.hist(arr, 50, density=True, range=(-1, 1))
     plt.axvline(x=0, color='black')
-    plt.xlabel('correlation')
+    plt.xlabel('Corr(q)')
     plt.ylabel('N')
     plt.xlim(-1, 1)
-    plt.title('The correlation of game similarity between\n'+name_1+' and '+name_2)
-    plt.savefig('correlation_'+name_1+'_'+name_2)
+    # plt.title('The correlation of game similarity between\n'+name_1+' and '+name_2)
+    plt.savefig('correlation_'+name_1+'_'+name_2+'.pdf')
 
 if __name__ == '__main__': 
     matrix_seller = np.load('seller_game_similarity.npy')
@@ -30,40 +30,46 @@ if __name__ == '__main__':
     matrix_standard = np.load('standard_game_similarity.npy')
     matrix_purchase = np.load('seller_purchase_game_similarity.npy')
     matrix_cop_standard = np.load('co-purchase_game_similarity.npy')
-    """
-    d_sb = matricesDistance(matrix_seller, matrix_buyer)
-    d_st = matricesDistance(matrix_seller, matrix_standard)
-    d_bt = matricesDistance(matrix_buyer, matrix_standard)
-    d_ps = matricesDistance(matrix_purchase, matrix_seller)
-    d_pt = matricesDistance(matrix_purchase, matrix_standard)
-    d_pb = matricesDistance(matrix_purchase, matrix_buyer)
-    
-    c_sb = matricesCorrelation(matrix_seller, matrix_buyer)
-    c_st = matricesCorrelation(matrix_seller, matrix_standard)
-    c_bt = matricesCorrelation(matrix_buyer, matrix_standard)
-    c_ps = matricesCorrelation(matrix_purchase, matrix_seller)
-    c_pt = matricesCorrelation(matrix_purchase, matrix_standard)
-    c_pb = matricesCorrelation(matrix_purchase, matrix_buyer)
-    c_cp = matricesCorrelation(matrix_purchase, matrix_cop_standard)
-    c_cs = matricesCorrelation(matrix_seller, matrix_cop_standard)
-    c_ct = matricesCorrelation(matrix_cop_standard, matrix_standard)
-    c_cb = matricesCorrelation(matrix_cop_standard, matrix_buyer)
-    """
+
     matrix_list = [matrix_seller, matrix_buyer, matrix_standard, matrix_purchase, matrix_cop_standard]
     name_list = ['seller-centric', 'buyer-centric', 'co-play standard', 'seller-centric with co-purchase input', 'co-purchase standard']
+    corr_list = []
     for i in range(len(matrix_list)): 
         for j in range(i+1, len(matrix_list)): 
-            corr = matricesCorrelation(matrix_list[i], matrix_list[j])
-            corrDistribution(corr, name_list[i], name_list[j])
+            corr_list.append(matricesCorrelation(matrix_list[i], matrix_list[j]))
+            # corrDistribution(corr, name_list[i], name_list[j])
+    
+    corr_plot = []
+    corr_plot.append(corr_list[3])
+    corr_plot.append(corr_list[9])
+    corr_plot.append(corr_list[8])
+    corr_plot.append(corr_list[1])
+    corr_plot.append(corr_list[7])
+    corr_plot.append(corr_list[4])
+
+    num_list = ['(a)', '(b)', '(c)', '(d)', '(e)', '(f)']
+    fig, axs = plt.subplots(nrows = 2, ncols = 3, figsize=(16, 8))
+    fig.subplots_adjust(hspace=0)
+    fig.subplots_adjust(wspace=0)
+    for corr, ax, num in zip(corr_plot, axs.ravel(), num_list): 
+        ax.hist(corr, 50, density=True, range=(-1, 1))
+        ax.set_xlim(-1.2, 1.2)
+        ax.set_ylim(0, 6.5)
+        ax.text(-1.1, 6, num, size=16)
+        if (num == num_list[3] or num == num_list[4] or num == num_list[5]): 
+            ax.set_xlabel('correlation', fontsize=16)
+        if (num == num_list[0] or num == num_list[3]): 
+            ax.set_ylabel('N', fontsize=16)
+            ax.tick_params(direction="in", which='both', labelsize=16)
+        else: 
+            ax.tick_params(direction="in", which='both', labelsize=16, labelleft=False)
+        ax.axvline(x=0, color='black')
+    plt.savefig('correlation.pdf')
     """
-    corr_list = [c_sb, c_st, c_bt, c_ps, c_pt, c_pb, c_cp, c_cs, c_ct, c_cb]
-    name_list = ['seller-centric and buyer-centric', 'seller-centric and co-play standard', 'buyer-centric and co-play standard', 'seller-centric and seller-centric with co-purchase input', 'seller-centric with co-purchase input and co-play standard', 'buyer-centric and seller-centric with co-purchase input', 'seller-centric with co-purchase input and co-purchase standard', 'seller-centric and co-purchase standard', 'co-purchase standard and co-play standard', 'buyer-centric and co-purchase standard']
-    for corr, name in zip(corr_list, name_list): 
-        corrDistribution(corr, name)
-    corrDistribution(c_sb, 'sb')
-    corrDistribution(c_st, 'st')
-    corrDistribution(c_bt, 'bt')
-    corrDistribution(c_ps, 'ps')
-    corrDistribution(c_pt, 'pt')
-    corrDistribution(c_pb, 'pb')
+    spec = np.loadtxt('similarity_tags.txt')
+    spec_cop = np.loadtxt('purchaseSimilarity_tags.txt')
+    for i in range(len(spec)): 
+        spec[i, i] = 0
+    corr = matricesCorrelation(spec, spec_cop)
+    corrDistribution(corr, 'seller-centric tag', 'seller-centric with co-purchase input tag')
     """
