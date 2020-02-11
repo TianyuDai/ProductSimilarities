a8aae73ecaa9d6402490a26b91a0e88eb16ada53
TianyuDai <td115@duke.edu>
2020-02-11 04:21:33 -0500

    update

--- steam/src/tianyu_product_graph/matrices_similarity.py
+++ steam/src/tianyu_product_graph/matrices_similarity.py
@@ -6,17 +6,17 @@ def matricesCorrelation(matrix_1, matrix_2):
     corr_list = []
     # print(matrix_1.shape)
     for i in range(len(matrix_1)): 
-        v_1 = matrix_1[i, :]
-        v_2 = matrix_2[i, :]
+        v_1 = matrix_1[i]
+        v_2 = matrix_2[i]
         # print(v_1.shape, matrix_1[i].shape)
-        # vector_1 = np.argsort(np.argsort(v_1))
-        # vector_2 = np.argsort(np.argsort(v_2))
-        vector_1 = rankdata(v_1)
-        vector_2 = rankdata(v_2)
-        # corr = np.corrcoef(vector_1, vector_2)
-        r, _ = pearsonr(vector_1, vector_2)
-        # corr_list.append(corr[0, 1])
-        corr_list.append(r)
+        vector_1 = np.argsort(np.argsort(v_1))
+        vector_2 = np.argsort(np.argsort(v_2))
+        # vector_1 = rankdata(v_1)
+        # vector_2 = rankdata(v_2)
+        corr = np.corrcoef(vector_1, vector_2)
+        # r, _ = pearsonr(vector_1, vector_2)
+        corr_list.append(corr[0, 1])
+        # corr_list.append(r)
         # print(r)
     return corr_list
 
@@ -43,7 +43,7 @@ if __name__ == '__main__':
     matrix_copurchase = np.load('shiyu_copurchase_similarity.npy')
     matrix_coplay = np.load('shiyu_coplay_similarity.npy')
     # matrix_cop_standard = np.load('co-purchase_game_similarity.npy')
-     
+    """ 
     for i in range(len(matrix_seller)): 
         # print(matrix_seller[i, i], matrix_seller_copurchase[i, i])
         matrix_seller[i, i] = 0
@@ -97,5 +97,5 @@ if __name__ == '__main__':
     plt.ylabel('N')
     plt.xlim(-1, 1)
     # plt.title('The correlation of game similarity between\n'+name_1+' and '+name_2)
-    plt.savefig('correlation_buyer_copurchase')
-    """
+    plt.savefig('correlation_buyer_copurchase.pdf')
+    
